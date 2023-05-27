e7c350b6fe3ca9c677c9f19ea8d88526d94518e8
Buyaki01 <rittahbuyaki@gmail.com>
2023-05-27 20:15:11 +0300

    Add route to post products

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
+const Product = require('./models/Product')
 
 app.use(cors())
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
+
+app.post('/products', async (req, res) => {
+  const { sku, name, price } = req.body
+  const productDoc = await Product.create({
+    sku,
+    name,
+    price
+  })
+  res.json(productDoc)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
